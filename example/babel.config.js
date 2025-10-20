b3ae1afbf50de2268ae2c91b1e32270c16066355
Kesha Antonov <innokenty.longway@gmail.com>
2025-10-20 11:49:22 +0300

    upd example to latest RN & deps

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,6 +1,32 @@
-module.exports = {
-  presets: [
-    'module:@react-native/babel-preset',
-    '@babel/preset-typescript',
-  ],
+module.exports = function (api) {
+  api.cache(true)
+
+  return {
+    presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
+    overrides: [
+      {
+        exclude: /\/node_modules\//,
+        plugins: [
+          [
+            'module-resolver',
+            {
+              extensions: ['.tsx', '.ts', '.js', '.json'],
+              alias: {
+                '@kesha-antonov/react-native-background-downloader': '../src',
+              },
+            },
+          ],
+        ],
+      },
+    ],
+    plugins: [
+      [
+        'babel-plugin-react-compiler',
+        {
+          target: '19',
+        },
+      ],
+      '@babel/plugin-proposal-export-namespace-from',
+    ],
+  }
 }
