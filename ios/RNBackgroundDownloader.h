4cfac42ead3e2ca1a9212dbb0c50c5da00759d33
Kesha Antonov <innokenty.longway@gmail.com>
2025-10-27 19:08:24 +0300

    rm new arch

--- ios/RNBackgroundDownloader.h
+++ ios/RNBackgroundDownloader.h
@@ -1,20 +1,10 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
-#ifdef RCT_NEW_ARCH_ENABLED
-#import <RNBackgroundDownloaderSpec/RNBackgroundDownloaderSpec.h>
-#endif
 
 typedef void (^CompletionHandler)();
 
 @interface RNBackgroundDownloader : RCTEventEmitter <RCTBridgeModule, NSURLSessionDelegate, NSURLSessionDownloadDelegate>
 
 + (void)setCompletionHandlerWithIdentifier:(NSString *)identifier completionHandler:(CompletionHandler)completionHandler;
-- (void)completeHandler:(NSString *)jobId resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject;
 
 @end
-
-#ifdef RCT_NEW_ARCH_ENABLED
-@interface RNBackgroundDownloader () <RNBackgroundDownloaderSpec>
-
-@end
-#endif
